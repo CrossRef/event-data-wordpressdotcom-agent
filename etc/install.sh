84510c8d4df3e5763304a5b723dd73f0497d2d39
Joe Wass <jwass@crossref.org>
2016-06-30 12:30:43 +0100

    Deploy script

--- etc/install.sh
+++ etc/install.sh
@@ -1,4 +1,13 @@
 # to be run from base directory
-lein uberjar
+set -e
+echo "Compile JAR"
+sudo -u deploy lein uberjar
+echo "Install service"
 cp etc/*.service /etc/systemd/system/
+echo "Reload services"
 systemctl daemon-reload
+echo "Restart service"
+service event-data-wordpressdotcom-agent-monitor restart
+service event-data-wordpressdotcom-agent-process restart
+service event-data-wordpressdotcom-agent-push restart
+echo "Done!"
